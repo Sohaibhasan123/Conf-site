636553ca2536dba9b9507e0f1560eba43e6ecb10
Shongkor <shongkortalukdar2017@gmail.com>
2023-08-12 09:26:42 +0600

    Sweet alert added for paper submission

--- coferense-portal-client/src/Component/AuthorComponent/PaperSubmission/Form.js
+++ coferense-portal-client/src/Component/AuthorComponent/PaperSubmission/Form.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import  "./Form.css";
+import "./Form.css";
 import PaperInfo from "./PaperInfo";
 import FilesUpload from "./FilesUpload";
 import AuthorInfo from "./AuthorInfo";
 import { MdOutlineArrowForwardIos } from 'react-icons/md';
 import { IoIosArrowBack } from 'react-icons/io'
-import {useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
+import Swal from 'sweetalert2'
 
 
 function Form() {
@@ -39,15 +39,15 @@ function Form() {
       address: "",
       affiliation: '',
     }
-    let author = [...formData['author'],object]
-     setFormData({...formData,author})
+    let author = [...formData['author'], object]
+    setFormData({ ...formData, author })
     console.log(formData);
   }
   const handleFormChange = (event, index) => {
-    
-     let data = [...formData['author']];
-     data[index][event.target.name] = event.target.value;
-     console.log(data);
+
+    let data = [...formData['author']];
+    data[index][event.target.name] = event.target.value;
+    console.log(data);
   }
   const PageDisplay = () => {
     if (page === 0) {
@@ -73,15 +73,15 @@ function Form() {
 
   return (
     <div className="form-container container m-4">
-      
+
       <div className="">
         <div className="header">
           <h1>{FormTitles[page]}</h1>
           <div className="progressbar">
-        <div
-          style={{ width: page === 0 ? "33.3%" : page == 1 ? "66.6%" : "100%" }}
-        ></div>
-      </div>
+            <div
+              style={{ width: page === 0 ? "33.3%" : page == 1 ? "66.6%" : "100%" }}
+            ></div>
+          </div>
         </div>
         <div className="body">{PageDisplay()}</div>
         <div className="footer my-4">
@@ -91,58 +91,72 @@ function Form() {
               setPage((currPage) => currPage - 1);
             }}
           >
-          <IoIosArrowBack />  Prev
+            <IoIosArrowBack />  Prev
           </button>
           <button
             onClick={() => {
               if (page === FormTitles.length - 1) {
-                const proceed = window.confirm("Are You Sure? You Want To Submit")
+                // const proceed = window.confirm("Are You Sure? You Want To Submit")
 
-                if(proceed){
-                  const Data = new FormData();
 
-                  Data.append( 'title', formData.title)
-                  Data.append( 'abstract', formData.abstract)
-                  Data.append( 'keywords', formData.keywords)
-                  Data.append( 'fileURL', formData.fileURL)
-                  formData.author.forEach(function (element,index) {
-                    Data.append(`author[${index}][firstName]`, element.firstName);
-                    Data.append(`author[${index}][lastName]`, element.lastName);
-                    Data.append(`author[${index}][email]`, element.email);
-                    Data.append(`author[${index}][country]`, element.country);
-                    Data.append(`author[${index}][address]`, element.address);
-                    Data.append(`author[${index}][affiliation]`, element.affiliation);
-                })
-                  // Data.append( 'author', JSON.stringify(formData.author))
+                Swal.fire({
+                  title: 'Are you sure?',
+                  text: "You won't be able to revert this!",
+                  icon: 'warning',
+                  showCancelButton: true,
+                  confirmButtonColor: '#008000',
+                  cancelButtonColor: '#d33',
+                  confirmButtonText: 'Yes !'
+                }).then((result) => {
+                  if (result.isConfirmed) {
+
+                    const Data = new FormData();
+
+                    Data.append('title', formData.title)
+                    Data.append('abstract', formData.abstract)
+                    Data.append('keywords', formData.keywords)
+                    Data.append('fileURL', formData.fileURL)
+                    formData.author.forEach(function (element, index) {
+                      Data.append(`author[${index}][firstName]`, element.firstName);
+                      Data.append(`author[${index}][lastName]`, element.lastName);
+                      Data.append(`author[${index}][email]`, element.email);
+                      Data.append(`author[${index}][country]`, element.country);
+                      Data.append(`author[${index}][address]`, element.address);
+                      Data.append(`author[${index}][affiliation]`, element.affiliation);
+                    })
+                    // Data.append( 'author', JSON.stringify(formData.author))
 
                     axios.post('http://localhost:8080/api/v1/submit', Data)
-                    .then(function (response) {
-                      Swal.fire({
-                        position: 'top-end',
-                        icon: 'success',
-                        title: 'Reviewer Added SuccessFully',
-                        showConfirmButton: false,
-                        timer: 1500
+                      .then(function (response) {
+                        Swal.fire(
+                          'Uploaded!',
+                          'Successfully Submitted!',
+                          'success'
+                        )
+                         navigate('/history');
                       })
-                        navigate('/history');
-                    })
-                    .catch(function (error) {
-                        console.log(error);
+                      .catch(function (error) {
                         Swal.fire({
                           position: 'top-end',
                           icon: 'error',
-                          title: 'Paper Submission Failed!',
+                          title: 'Paper Submission Failed',
                           showConfirmButton: false,
                           timer: 1500
                         })
-                    });
-                }
+                      });
+                  }
+                })
+
+
+                // if (proceed) {
+                //   
+                // }
               } else {
                 setPage((currPage) => currPage + 1);
               }
             }}
           >
-            {page === FormTitles.length - 1 ? "Submit" : <>Next <MdOutlineArrowForwardIos/></>}
+            {page === FormTitles.length - 1 ? "Submit" : <>Next <MdOutlineArrowForwardIos /></>}
           </button>
         </div>
       </div>
